e5e424c5d9224b01a59734703693ae7e23c43e9d
Nguyen Dinh Thuan <thuan.nguyendinh@hust.edu.vn>
2024-11-20 13:10:32 +0700

    first commit

---
+++ app/src/main/java/com/example/androidviewmodel/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.androidviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+
+class MainActivity : AppCompatActivity() {
+    var point: Int=0
+    val viewModel: ScoreViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.btnIncrease)
+        var txt = findViewById<TextView>(R.id.txtPoint)
+//        btn.setOnClickListener {
+//            point++
+//            txt.text=point.toString()
+//        }
+        btn.setOnClickListener {
+            viewModel.incrementScore()
+            txt.text = viewModel.point.toString()
+        }
+    }
+}
+class ScoreViewModel : ViewModel() {
+    var point : Int = 0
+    fun incrementScore() {
+      point++
+    }
+}
